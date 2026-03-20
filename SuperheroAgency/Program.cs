cadf02d698c36da0a6d4cd503dcd451c0301b619
tomlenton <twlenton@gmail.com>
2026-03-20 11:15:27 +0000

    Add GetLast

--- SuperheroAgency/Program.cs
+++ SuperheroAgency/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] intArray = { 1, 5, 2, 8, 6 };
+            Console.WriteLine(GetLast(intArray)); // 6
+
+            double[] doubleArray = { 10.5, 60.5, 2.2, 8.76, 6.1111 };
+            Console.WriteLine(GetLast(doubleArray)); // 6.1111
+
+            string[] stringArray = { "hello", "world", "it's", "northcoders!" };
+            Console.WriteLine(GetLast(stringArray)); // "northcoders"
         }
+        public static T GetLast<T>(T[] array) => array[array.Length - 1];
+
     }
 }
